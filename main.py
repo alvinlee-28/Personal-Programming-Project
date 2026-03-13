ba030fe76c9140874d2687ca2202ecae4d1e58aa
alvinlee-28 <alee28@kellettschool.com>
2026-03-13 06:57:00 +0000

    Mar 13, 2026, 2:57 PM

--- main.py
+++ main.py
@@ -1,39 +1,39 @@
 ## Personal Programming Project - Alvin Lee
 
 # store letters values
-# freq = {"E": 12,
-#        "A": 9,
-#        "I": 9,
-#        "O": 8,
-#        "N": 6,
-#        "R": 6,
-#        "T": 6,
-#        "L": 4,
-#        "S": 4,
-#        "U": 4,
-#        "D": 4,
-#        "G": 3,
-#        "B": 2,
-#        "C": 2,
-#        "M": 2,
-#        "P": 2,
-#        "F": 2,
-#        "H": 2,
-#        "V": 2,
-#        "W": 2,
-#        "Y": 2,
-#        "K": 1,
-#        "J": 1,
-#        "X": 1,
-#        "Q": 1,
-#        "Z": 1}
-# points = {["E", "A", "I", "O", "N", "R", "T", "L", "S", "U"]: 1,
-#          ["D", "G"]: 2,
-#          ["B", "C", "M", "P"]: 3,
-#          ["F", "H", "V", "W", "Y"]: 4,
-#          ["K"]: 5,
-#          ["J", "X"]: 8,
-#          ["Q", "Z"]: 10}
+freq = {"E": 12,
+       "A": 9,
+       "I": 9,
+       "O": 8,
+       "N": 6,
+       "R": 6,
+       "T": 6,
+       "L": 4,
+       "S": 4,
+       "U": 4,
+       "D": 4,
+       "G": 3,
+       "B": 2,
+       "C": 2,
+       "M": 2,
+       "P": 2,
+       "F": 2,
+       "H": 2,
+       "V": 2,
+       "W": 2,
+       "Y": 2,
+       "K": 1,
+       "J": 1,
+       "X": 1,
+       "Q": 1,
+       "Z": 1}
+points = {["E", "A", "I", "O", "N", "R", "T", "L", "S", "U"]: 1,
+         ["D", "G"]: 2,
+         ["B", "C", "M", "P"]: 3,
+         ["F", "H", "V", "W", "Y"]: 4,
+         ["K"]: 5,
+         ["J", "X"]: 8,
+         ["Q", "Z"]: 10}
 
 def update_letters():
     pass
@@ -56,8 +56,10 @@ def player_first_turn():
 def introduction():
     print("WELCOME TO SCRABBLE!!")
     print("Enter turn with letter and position of letter (e.g. J B1)\n")
+    print("wert")
 
 def main():
     introduction()
 
+print(True)
 main()
