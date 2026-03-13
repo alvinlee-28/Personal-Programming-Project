2388c2b124cf0de17917a7d1f5af3ec35db9da9d
alvinlee-28 <alee28@kellettschool.com>
2026-03-13 06:58:05 +0000

    Mar 13, 2026, 2:58 PM

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 ## Personal Programming Project - Alvin Lee
+import docx
+from docx.enum.text import WD_COLOR_INDEX
 
 # store letters values
 freq = {"E": 12,
@@ -27,13 +29,13 @@ freq = {"E": 12,
        "X": 1,
        "Q": 1,
        "Z": 1}
-points = {["E", "A", "I", "O", "N", "R", "T", "L", "S", "U"]: 1,
-         ["D", "G"]: 2,
-         ["B", "C", "M", "P"]: 3,
-         ["F", "H", "V", "W", "Y"]: 4,
-         ["K"]: 5,
-         ["J", "X"]: 8,
-         ["Q", "Z"]: 10}
+# points = {["E", "A", "I", "O", "N", "R", "T", "L", "S", "U"]: 1,
+#          ["D", "G"]: 2,
+#          ["B", "C", "M", "P"]: 3,
+#          ["F", "H", "V", "W", "Y"]: 4,
+#          ["K"]: 5,
+#          ["J", "X"]: 8,
+#          ["Q", "Z"]: 10}
 
 def update_letters():
     pass
@@ -56,10 +58,9 @@ def player_first_turn():
 def introduction():
     print("WELCOME TO SCRABBLE!!")
     print("Enter turn with letter and position of letter (e.g. J B1)\n")
-    print("wert")
+    print("hi".font.highlight_color == WD_COLR_INDEX.YELLOW)
 
 def main():
     introduction()
 
-print(True)
 main()
