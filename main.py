a8374e517088ac1c8b89318c0d06fe4b93bd8703
alvinlee-28 <alee28@kellettschool.com>
2026-03-26 03:00:21 +0000

    Mar 26, 2026, 11:00 AM

--- main.py
+++ main.py
@@ -200,6 +200,18 @@ def main():
         plr_words = wordsleft
 
 
+from PyDictionary import PyDictionary
+
+dictionary=PyDictionary("hotel","ambush","nonchalant","perceptive")
+'There can be any number of words in the Instance'
+
+print(dictionary.printMeanings()) '''This print the meanings of all the words'''
+print(dictionary.getMeanings()) '''This will return meanings as dictionaries'''
+print (dictionary.getSynonyms())
+
+print (dictionary.translateTo("hi")) '''This will translate all words to Hindi'''
+
+
 print (dictionary.antonym("Life"))
 x = False
 y = [['E', 'A3'], ['T', 'A1'], ['I', 'A2'], ['Y', 'A4']]
